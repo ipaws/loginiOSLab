4d3b3ffe573a36953df4efa7a1084c2c7609740a
Paw <uglypawis@hotmail.com>
2018-02-22 18:54:36 -0600

    Initial Commit

---
+++ loginLab/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  loginLab
+//
+//  Created by iPaw on 22/02/18.
+//  Copyright © 2018 iPaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    
+   
+    @IBOutlet weak var buttonLog: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        
+    buttonLog.layer.cornerRadius = 10 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
