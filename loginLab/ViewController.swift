6de0f8b428079d756286ae9b3c477557da865a7b
Paw <uglypawis@hotmail.com>
2018-02-26 16:02:52 -0600

    :)

--- loginLab/ViewController.swift
+++ loginLab/ViewController.swift
@@ -13,20 +13,27 @@ class ViewController: UIViewController {
 
     
    
+
     @IBOutlet weak var buttonLog: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
         
-    buttonLog.layer.cornerRadius = 10 
+    buttonLog.layer.cornerRadius = 10 //modifica el borde del boton
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+        
+    } //Oculta el teclado cuando se presiona intro
 
 }
 
